7f1eb10ad83eb74f915da46c58ff85a3f682c29f
Encrefy <68856095+AboodeDaBOSS@users.noreply.github.com>
2024-08-13 14:09:05 +0400

    Updated custom-misp.py

--- custom-misp.py
+++ custom-misp.py
@@ -7,14 +7,20 @@ from socket import socket, AF_UNIX, SOCK_DGRAM
 from datetime import date, datetime, timedelta
 import time
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning  # Add this import
 from requests.exceptions import ConnectionError
 import json
 import ipaddress
 import hashlib
 import re
 
+# Suppress SSL warnings-------------
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # Add this line
+
 pwd = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 socket_addr = "{0}/queue/sockets/queue".format(pwd)
+
+# Rest of your script...
 
 
 def send_event(msg, agent=None):
@@ -31,20 +37,21 @@ def send_event(msg, agent=None):
     sock.connect(socket_addr)
     sock.send(string.encode())
     sock.close()
-
-
+    print("Sent Event:", string)  # Add this line---------------
 false = False
 # Read configuration parameters
 alert_file = open(sys.argv[1])
 # Read the alert file
 alert = json.loads(alert_file.read())
 alert_file.close()
+print("Alert Data:", alert)  # Add this line----------------------
 # New Alert Output if MISP Alert or Error calling the API
 alert_output = {}
+print("Alert Output:", alert_output)  # Add this line----------------
 # MISP Server Base URL
-misp_base_url = "https://**your misp instance**/attributes/restSearch/"
+misp_base_url = "https://192.168.253.145/attributes/restSearch/"
 # MISP Server API AUTH KEY
-misp_api_auth_key = "*Your API Key"
+misp_api_auth_key = "zQYt3pUk8l20rD8g8CU0DvZPT5P6zu7Pyl3ZwyR8"
 # API - HTTP Headers
 misp_apicall_headers = {
     "Content-Type": "application/json",
@@ -132,16 +139,20 @@ if event_source == "windows":
         sys.exit()
     misp_search_value = "value:" f"{wazuh_event_param}"
     misp_search_url = "".join([misp_base_url, misp_search_value])
+    print("MISP Search URL:", misp_search_url)  # Add this line----------
     try:
         misp_api_response = requests.get(
             misp_search_url, headers=misp_apicall_headers, verify=False
         )
+        print("MISP API Status Code:", misp_api_response.status_code)  # Add this line-----------------
+        print("MISP API Response Headers:", misp_api_response.headers)  # Add this line--------------------
     except ConnectionError:
         alert_output["misp"] = {}
         alert_output["integration"] = "misp"
         alert_output["misp"]["error"] = "Connection Error to MISP API"
         send_event(alert_output, alert["agent"])
     else:
+        print("MISP API Response:", misp_api_response.text)  # Add this line-------------
         misp_api_response = misp_api_response.json()
         # Check if response includes Attributes (IoCs)
         if misp_api_response["response"]["Attribute"]:
@@ -161,7 +172,7 @@ if event_source == "windows":
             alert_output["misp"]["type"] = misp_api_response["response"]["Attribute"][
                 0
             ]["type"]
-            alert_output["misp"]["source"]["description"] = alert["rule"]["description"]
+            alert_output["misp"]["source"]["description"] = alert["rule"].get("description", "No description available")
             send_event(alert_output, alert["agent"])
 elif event_source == "linux":
     if (
